8a1c40f8892970039b2e84afbf76b772b4229662
Leonardo Calvet <leonardofmcalvet@gmail.com>
2022-08-01 20:33:54 -0300

    feat: Criação de um arq js para as ações

---
+++ javascripts/functions.js
@@ -0,0 +1,25 @@
+// Antigo adocaoLobinho.js
+
+// let url_site = "https://lobinhos.herokuapp.com/wolves"
+
+
+// function adoption_request() {
+//     let fetch_config = {
+//     method: "PATCH"
+// }
+    
+// }
+
+
+// let adotar_button = document.querySelector(".button_adotar")
+// adotar_button.addEventListener("click", adoption_request)
+
+// // fetch(url_site, fetch_config)
+// //     .then(resposta => resposta.json()
+// //         .then(resp => adoption_request())
+// //         .catch(resp => { console.log("Ok do Erro") })
+// //     )
+// //     .catch(resposta => { console.log("Erro") })
+// // fetch retorna uma Promise
+// // Possíveis retornos: Pendente (.then) | Concluida (.then | Rejeitada (.catch)
+
