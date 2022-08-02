365994d0f42c116bfb314706553562acc31eff95
Leonardo Calvet <leonardofmcalvet@gmail.com>
2022-08-02 16:33:06 -0300

    feat: Feito código Adoção de Lobo

--- javascripts/adocaoLobinho.js
+++ javascripts/adocaoLobinho.js
@@ -1,4 +1,4 @@
-const url_site = "https://lobinhos.herokuapp.com/wolves" // url do site que pegará os dados
+const url_site = "https://lobinhos.herokuapp.com/wolves/282" // url do site que pegará os dados
 
 
 function adopt_lobinho(){
@@ -6,9 +6,30 @@ function adopt_lobinho(){
     let adoptor_age = document.querySelector("#age").value
     let adoptor_mail = document.querySelector("#email").value
 
-    console.log(adoptor_name);
-    console.log(adoptor_age);
-    console.log(adoptor_mail);
+    // console.log(adoptor_name);
+    // console.log(adoptor_age);
+    // console.log(adoptor_mail);
+
+    let fetch_body = {
+        adopter_name: adoptor_name,
+        adopter_email: adoptor_mail,
+        adopter_age: adoptor_age,
+        adopted: true
+    }
+
+    let fetch_config = {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(fetch_body)
+    }
+
+    fetch(url_site, fetch_config)
+        .then(resposta => resposta.json()
+            .then(resp => { console.log(resp) })
+            .catch(error => { console.log(error) }))
+        .catch(error => { console.log(error) })
+
+
 
 }
 
