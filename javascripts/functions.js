0394551553eedd6f922faf6fa77472d3a6d29347
Leonardo Calvet <leonardofmcalvet@gmail.com>
2022-08-02 14:31:29 -0300

    feat: Adicionar um lobo para o banco de dados

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -7,7 +7,7 @@
 //     let fetch_config = {
 //     method: "PATCH"
 // }
-    
+
 // }
 
 
@@ -23,3 +23,39 @@
 // // fetch retorna uma Promise
 // // Possíveis retornos: Pendente (.then) | Concluida (.then | Rejeitada (.catch)
 
+const url_site = "https://lobinhos.herokuapp.com/wolves" // url do site que pegará os dados
+
+function lobinho_login() {
+    let new_name_lobinho = document.querySelector("#name_c").value
+    let new_age_lobinho = document.querySelector("#age_c").value
+    let new_photo_lobinho = document.querySelector("#link_c").value
+    let new_description_lobinho = document.querySelector("#description").value
+
+    // console.log(new_name_lobinho);
+    // console.log(new_age_lobinho);
+    // console.log(new_photo_lobinho);
+    // console.log(new_description_lobinho);
+
+    // vairável que coterá as informasções que serão colocadas no bd
+    let fetch_body = {
+        name: new_name_lobinho,
+        age: new_age_lobinho,
+        image_url: new_photo_lobinho,
+        description: new_description_lobinho
+    }
+    
+    let fetch_config = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(fetch_body)
+    }
+
+    fetch(url_site, fetch_config)
+        .then(resposta => resposta.json()
+            .then(resp => { console.log(resp)})
+            .catch(error => {console.log(error)}))
+        .catch(error => {console.log(error)})
+
+}
+let do_lobinho_login = document.querySelector(".button_adotar");
+do_lobinho_login.addEventListener("click", lobinho_login);
